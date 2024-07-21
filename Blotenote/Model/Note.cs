e49e21d1d12589ace0693488f34822d27cda0b03
Bally <Bally.Lomibao@howickltd.com>
2024-07-21 12:57:40 +0100

    renamed props

--- Blotenote/Model/Note.cs
+++ Blotenote/Model/Note.cs
@@ -12,8 +12,8 @@ namespace Blotenote.Model
         [Indexed]
         public int NotebookId { get; set; }
         public string Title { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime UpdatedTime { get; set; }
         public string FileLocation { get; set; }
     }
 }
